3eb6984423b8fb4f725d77db09753c0403b546fd
Gentiana Desipojci <gentiana.desipojci@gmail.com>
2016-09-30 21:35:50 -0500

    Updating create and destroy env scripts
    Updating create-env script according to updated assignement.
    Added loops in destroy-env script

--- create-env.sh
+++ create-env.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-06b94666 --key-name devenv-key --security-group-id sg-43da1f3a --instance-type t2.micro --user-data file://installapp.sh --count $1 --placement AvailabilityZone=us-west-2b
-launchedInstanceIds="$(aws ec2 describe-instances --query 'Reservations[*].Instances[].InstanceId')"
 loadBalancerName='itmo544-load-balancer'
-aws ec2 wait instance-running --instance-ids $launchedInstanceIds
 aws elb create-load-balancer --load-balancer-name $loadBalancerName --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --security-groups sg-43da1f3a --availability-zones us-west-2b
 
-aws elb register-instances-with-load-balancer --load-balancer-name $loadBalancerName --instances $launchedInstanceIds
 launchConfigurationName='webserver-config'
-aws autoscaling create-launch-configuration --launch-configuration-name $launchConfigurationName --image-id ami-06b94666 --key-name devenv-key --instance-type t2.micro --user-data file://installapp.sh 
+aws autoscaling create-launch-configuration --launch-configuration-name $launchConfigurationName --image-id $1 --key-name devenv-key --instance-type t2.micro --user-data file://installapp.sh 
 autoScalingGroupName='my-autoscaling-webserver'
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name $autoScalingGroupName --launch-configuration-name $launchConfigurationName --availability-zones "us-west-2b" --load-balancer-name $loadBalancerName --max-size 5 --min-size 2 --desired-capacity 4
--- destroy-env.sh
+++ destroy-env.sh
@@ -1,40 +1,66 @@
 #!/bin/bash
 
-launchedInstanceIds="$(aws ec2 describe-instances --query 'Reservations[*].Instances[].InstanceId')"
-echo "Instances  $launchedInstanceIds"
+autoScalingGroupNames="$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName')"
+echo "Auto-scaling-groups  $autoScalingGroupNames"
 
-autoScalingGroupName="$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[0].AutoScalingGroupName')"
-echo "Auto-scaling-groups  $autoScalingGroupName"
-
-launchConfigurationName="$(aws autoscaling describe-launch-configurations --query 'LaunchConfigurations[0].LaunchConfigurationName')"
-echo "launch-configurations  $launchConfigurationName"
+launchConfigurationNames="$(aws autoscaling describe-launch-configurations --query 'LaunchConfigurations[*].LaunchConfigurationName')"
+echo "launch-configurations  $launchConfigurationNames"
 
 
 loadBalancerNames="$(aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].LoadBalancerName')"
 echo "load-balancers  $loadBalancerNames"
 
+#Loop foreach auto-scaling-group
+autoScalingGroupArray=($autoScalingGroupNames)
+for i in ${autoScalingGroupArray[@]};
+do 
 #Detach load-balancers from autoscaling group
-aws autoscaling detach-load-balancers --load-balancer-names $loadBalancerNames --auto-scaling-group-name $autoScalingGroupName
-
+ loadBalancerNamesPerAutoScalingGr=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?AutoScalingGroupName=='$i'].LoadBalancerNames");
+echo "Detach load-balancers from autoscaling group:  $i";
+aws autoscaling detach-load-balancers --load-balancer-names $loadBalancerNamesPerAutoScalingGr --auto-scaling-group-name $i;
 #Delete auto-scaling-group 
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $autoScalingGroupName --force-delete
+echo "Delete auto-scaling-group: $i"
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $i --force-delete
+ done;
 
-#Delete launch-configuration
-aws autoscaling delete-launch-configuration --launch-configuration-name $launchConfigurationName
+#Loop launch-configuration
+launchConfigurationArray=($launchConfigurationNames)
+for i in ${launchConfigurationArray[@]};
+do 
+echo "Deleting launch-configuration:  $i";
+ #Delete launch-configuration
+aws autoscaling delete-launch-configuration --launch-configuration-name $i
+ done;
+ 
+
+
+#Loop foreach load-balancer
+loadBalancersArray=($loadBalancerNames)
+
+for i in ${loadBalancersArray[@]};
+do 
 
 #Deregister instances from load-balancer
-aws elb deregister-instances-from-load-balancer --load-balancer-name $loadBalancerNames --instances $launchedInstanceIds
+ registeredInstancesPerLoadBalancer=$( aws elb describe-load-balancers --query "LoadBalancerDescriptions[?LoadBalancerName=='$i'].Instances[*].InstanceId");
+ echo "Deregister instances from load-balancer: $i"; 
+aws elb deregister-instances-from-load-balancer --load-balancer-name $i --instances $registeredInstancesPerLoadBalancer;
 
 #Delete load-balancer listeners 
-loadBalancerListeners="$( aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].ListenerDescriptions[*].Listener.InstancePort')"
-aws elb delete-load-balancer-listeners --load-balancer-name $loadBalancerNames --load-balancer-ports $loadBalancerListeners
-
-#Delete load-balancer policy -- To be done.
+loadBalancerListeners=$( aws elb describe-load-balancers --query "LoadBalancerDescriptions[?LoadBalancerName=='$i'].ListenerDescriptions[*].Listener.InstancePort");
+aws elb delete-load-balancer-listeners --load-balancer-name $i --load-balancer-ports $loadBalancerListeners;
 
 #Delete load-balancer 
-aws elb  delete-load-balancer --load-balancer-name $loadBalancerNames
+aws elb  delete-load-balancer --load-balancer-name $i;
 
 #Terminate instances
+aws ec2 terminate-instances --instance-ids $registeredInstancesPerLoadBalancer;
+ done;
+
+
+
+#Terminate remaining instances
+launchedInstanceIds="$(aws ec2 describe-instances --query 'Reservations[*].Instances[].InstanceId')"
+echo "Instances  $launchedInstanceIds"
 aws ec2 terminate-instances --instance-ids $launchedInstanceIds
 
 
